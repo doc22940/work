3324f3f19ae651470869ee7470883f83c154e805
‘Ammar’ <haleesammar@gmail.com>
2019-05-18 18:02:46 +0300

    Updates

---
+++ precache-manifest.988614a2464df2a5ed07ba0555756c40.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d192a5b2b2a78079bc470da8a46b477",
+    "url": "/work/index.html"
+  },
+  {
+    "revision": "d07be04e9cfe32697b0b",
+    "url": "/work/static/css/main.f347239f.chunk.css"
+  },
+  {
+    "revision": "0a664e7da05996247c5d",
+    "url": "/work/static/js/2.00afec89.chunk.js"
+  },
+  {
+    "revision": "d07be04e9cfe32697b0b",
+    "url": "/work/static/js/main.1c3c6ecc.chunk.js"
+  },
+  {
+    "revision": "2faa51981ada9eb26daf",
+    "url": "/work/static/js/runtime~main.84d4238b.js"
+  },
+  {
+    "revision": "9b076d9018c76721c30811e44bd7c72f",
+    "url": "/work/static/media/react.9b076d90.png"
+  },
+  {
+    "revision": "aa8c98f7461a29d51e9b22096e6eba43",
+    "url": "/work/static/media/slide01.aa8c98f7.jpg"
+  },
+  {
+    "revision": "a80fde2e0b1385a74cc7186942c4ff71",
+    "url": "/work/static/media/snap.a80fde2e.png"
+  }
+]);
