f766f205a7895cda58bb215559b28467d0308bb9
ammar <haleesammar@gmail.com>
2019-11-03 14:36:30 +0200

    favicon changed

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,8 +7,16 @@ import data from '../Data/ProjectData'
 const Projects = (props) => {
         return ( 
 
+<div style={{textAlign:"center"}}>
+
+<div style={{paddingBottom:"10px",color:"rgba(0, 0, 0, 0.64)",fontStyle:"italic"}}>
+<span style={{color:"black",fontSize:"bold"}}>{`*Please Note* `}</span>These are old projects are somewhat old
+
+</div>
 
                 <div id="lower_profile_3" >
+        
+       
 
                 {
                         data.map(project=>{
@@ -21,6 +29,9 @@ const Projects = (props) => {
                     
 
                 </div>
+
+</div >
+
 
 
          );
